c14094ea2897787185ae065a5ef7584f499bd66c
Ruben <ruben.dominguez.93@gmail.com>
2017-03-31 11:13:58 +0200

    changed

--- influx-cpu.py
+++ influx-cpu.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 import os
+import sys
+import subprocess
 import argparse
+
+#Es bastante cutrecillo, pero hace lo que se necesita (espero xD). Lo mejorare cuando pueda.
+#Disponible tambien en https://github.com/Loenberg123/influx-telegraf-cpu
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-Q", "--query", help="Realizar consulta", action="store_true")
@@ -13,8 +18,17 @@ parser.add_argument("-V", "--value", help="Valor del que se realiza la consulta"
 parser.add_argument("-t", "--time", help="Tiempo desde el que se obtienen los datos. Ns, Nm, Nh")
 args = parser.parse_args()
 
+
 if args.query:
-	query = os.system("curl -G "+ args.secure +"://"+ args.ip +":8086/query --data-urlencode db="+ args.database +" --data-urlencode epoch=s --data-urlencode \"q=SELECT mean("+ args.value +") FROM cpu WHERE host='"+ args.host +"' AND time > now() - "+args.time+" GROUP BY time(10m)\"")
+	q = "curl -G "+ args.secure +"://"+ args.ip +":8086/query --data-urlencode db="+ args.database +" --data-urlencode \"q=SELECT mean("+ args.value +") FROM cpu WHERE host='"+ args.host +"' AND time > now() - "+args.time+"\" | awk -F',' '{print $6}'| awk -F] '{print \"media: \" $1}'"
+	result = subprocess.check_output(q, shell=True)
+	print "Host "+args.host+" "+args.value+" "+result
+	sys.exit(0)
 
 if args.version:
-	version = os.system("curl -sl -I "+ args.secure +"://"+ args.ip +":8086/ping")
+	ver = "curl -sl -I "+ args.secure +"://"+ args.ip +":8086/ping | awk 'NR==4{print}'"
+	os.system(ver)
+	sys.exit(0)
+
+else:
+	sys.exit(2)
